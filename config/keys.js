5b266a2f75112832c43d7f7de1e66d15cf1f83b6
mtramine <amine.mehtar@hotmail.com>
2022-10-10 22:30:14 +0200

    Project reorganization file removal

--- config/keys.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-    MongoURI: 'mongodb+srv://mtramine:mtramine@cluster0.dcpclmo.mongodb.net/?retryWrites=true&w=majority',
-    JWT_SECRET_KEY: 'gt6g_jwt_secret_key_to_be_changed'
-}
